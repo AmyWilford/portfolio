76e7747be3961a0f911abc422ef7f70c1983961f
Amy Wilford <wilford.amy@gmail.com>
2021-08-16 12:11:28 -0400

    final commit less colored social icons in nav

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 //mobile navigation
 function myFunction() {
-    var x = document.getElementById("myLinks");
+    var x = document.getElementById("navLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
